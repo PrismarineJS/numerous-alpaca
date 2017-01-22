618060f4f9b7c051a102c19e744c7d3a72709ec3
mhsjlw <mhsjlw@users.noreply.github.com>
2017-01-22 11:35:30 -0500

    1.0

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function createPEServer(options) {
     options.name = 'Minecraft: PE Server';
   }
   const peServer = new PEServer();
-  options.name = 'MCPE;' + options.name + ';70 70;0.14.3;0;' + options['max-players'];
+  options.name = 'MCPE;' + options.name + ';100 100;1.0.0.0;0;' + options['max-players'];
   peServer.connect(options);
   return peServer;
 }
